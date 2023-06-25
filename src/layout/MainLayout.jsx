c610adee15109e3eb1faa7f7d37268ce675ccdf7
saidur-way <saiduronway@gmail.com>
2023-06-25 11:07:20 +0600

    initial setup

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Footer from "../pages/Footer/Footer";
+import Home from "../pages/Home/Home";
+
+
+const MainLayout = () => {
+     return (
+          <div>
+               <Home></Home>
+               <Footer></Footer>
+          </div>
+     );
+};
+
+export default MainLayout;
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+     return (
+          <div>
+               <h2 className="text-center text-blue-600 bg-black">This is footer!</h2>
+          </div>
+     );
+};
+
+export default Footer;
