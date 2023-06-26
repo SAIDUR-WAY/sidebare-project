dbd117a73efede965ef0292a346395151a7d9737
saidur-way <saiduronway@gmail.com>
2023-06-26 13:18:42 +0600

    side bare wide fixed

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar';
 const Sidebar = () => {
      const [open, setOpen] = useState(true)
      return (
-          <div className={`bg-[#081A51] text-white h-screen w-60 relative p-4 pt-8 ${open ? 'w-72' : 'w-16' } duration-300 `}>
+          <div className={`bg-[#081A51] text-white h-screen  relative p-4 pt-8 ${open ? 'w-72' : 'w-16' } duration-300 `}>
                <FaArrowLeft className={` bg-white rounded-full text-black absolute top-9 -right-3 border border-black text-2xl cursor-pointer ${!open && 'rotate-180'}`}  onClick={()=>setOpen(!open)} ></FaArrowLeft>
                <div className='inline-flex items-center justify-center gap-4 '>
                     <FaRegListAlt className= {`text-2xl cursor-pointer duration-500 ${!open && 'rotate-[360deg]'}`}></FaRegListAlt>
